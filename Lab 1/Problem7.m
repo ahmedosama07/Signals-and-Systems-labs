a9b8c1a64daedde5aeda6dea5c4ffe91b286bd9a
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-03-03 22:03:59 +0200

    Lab 1 Problem 7

---
+++ Lab 1/Problem7.m
@@ -0,0 +1,25 @@
+% 2x1  +  3x2  +  5x3  +  6x4  + 21x5  = 152
+% 5x1          +  2x3  +  2x4          =  19
+% 6x1  +  7x2  +  8x3  +  9x4  + 11x5  = 135
+%        13x2  + 17x3  +  5x4  +  6x5  = 127
+%  x1  +  4x2          +  3x4  +  9x5  =  66
+%A = zeros(5);
+B = zeros(5, 1);
+
+
+openvar('A')
+
+B = input('Enter the RHS inside [] separated by simicolons: ');
+ 
+augA = [A B];
+rankA = rank(A);
+rank_augA = rank(augA);
+S = rankA == 5;
+ 
+if S == 1
+    disp('Consistent');
+end
+A
+X = linsolve(A,B);
+
+X
