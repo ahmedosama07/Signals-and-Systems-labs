a2db8db7d101cd0ffb093c4dcc219c6c4f4442ce
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-04-26 23:58:13 +0200

    Update Problem3.m

--- Lab 2/Problem3.m
+++ Lab 2/Problem3.m
@@ -1,21 +1,19 @@
-n = zeros(1,100);
-for i=1:1:100
-    n(i)=i-1;
-end
+n = 0:100;
 
-Fs = 5000;
+Fs1 = 5e3;
 
-Fo_05 = 0.5*1000;
-Fo_2 = 2*1000;
-Fo_3 = 3*1000;
-Fo_45 = 4.5*1000;
+Fo_05 = 0.5e3;
+Fo_2 = 2e3;
+Fo_3 = 3e3;
+Fo_45 = 4.5e3;
 
-t= n./Fs;
+t= n./Fs1;
 x_05 = sin(2*pi*Fo_05.*t);
 x_2 = sin(2*pi*Fo_2.*t);
 x_3 = sin(2*pi*Fo_3.*t);
 x_45 = sin(2*pi*Fo_45.*t);
 
+figure(1);
 subplot(4,1,1);
 stem(t,x_05);
 subplot(4,1,2);
@@ -25,3 +23,19 @@ stem(t,x_3);
 subplot(4,1,4);
 stem(t,x_45);
 
+%% Part 2
+Fs2 = 50e3;
+
+Fo = 2e3;
+
+t1 = n./Fs2;
+t2 = t1(2:2:100);
+
+x = sin(2*pi*Fo.*t1);
+y = x(2:2:100);
+
+figure(2);
+subplot(2, 1, 1);
+stem(t1,x);
+subplot(2, 1, 2);
+stem(t2,y);
