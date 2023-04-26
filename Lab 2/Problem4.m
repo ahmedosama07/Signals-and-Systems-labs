c277cda9cd56c9f93cbb2d40afe2d7602019058a
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-04-27 00:08:57 +0200

    Create Problem4.m

---
+++ Lab 2/Problem4.m
@@ -0,0 +1,28 @@
+n = linspace(-40,40,20);
+t = linspace(-40,40);
+
+Xn = cos((2.*n)./3);
+Xt = cos((2.*t)./3);
+
+Yn = cos((8.*pi.*n)./38);
+Yt = cos((8.*pi.*t)./38);
+
+subplot(4,1,1)
+stem(n,Xn)
+xlabel('n')
+ylabel('x(n)')
+
+subplot(4,1,2)
+plot(t,Xt)
+xlabel('t')
+ylabel('x(t)')
+
+subplot(4,1,3)
+stem(n,Yn)
+xlabel('n')
+ylabel('y(n)')
+
+subplot(4,1,4)
+plot(t,Yt)
+xlabel('t')
+ylabel('y(t)')
