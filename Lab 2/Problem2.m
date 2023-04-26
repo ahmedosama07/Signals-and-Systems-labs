f5a7e8648b0dc9d294c403a71c92e8220f65d3d9
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-04-26 21:35:37 +0200

    Problem 2

---
+++ Lab 2/Problem2.m
@@ -0,0 +1,15 @@
+t = linspace(0,10);
+
+y1 = sin(t) ./ t;
+y2 = (1 ./ ((t - 1).^2)) + t;
+y3 = ((t.^2) + 1) / ((t.^2) - 4);
+y4 = (nthroot((10 - t), 3) - 2) / sqrt(4 - (t.^2));
+
+subplot(4,1,1);
+plot(t,y1);
+subplot(4,1,2); 
+plot(t,y2);
+subplot(4,1,3); 
+plot(t,y3);
+subplot(4,1,4); 
+plot(t,y4);
