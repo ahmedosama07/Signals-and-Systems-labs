3de7df39522e4cca0bf1e06a8d260e551af85166
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-04-27 20:57:40 +0200

    Update Problem4.m

--- Lab 2/Problem4.m
+++ Lab 2/Problem4.m
@@ -7,22 +7,22 @@ Xt = cos((2.*t)./3);
 Yn = cos((8.*pi.*n)./38);
 Yt = cos((8.*pi.*t)./38);
 
-subplot(4,1,1)
-stem(n,Xn)
-xlabel('n')
-ylabel('x(n)')
+subplot(4,1,1);
+stem(n,Xn);
+xlabel('n');
+ylabel('x(n)');
 
-subplot(4,1,2)
-plot(t,Xt)
-xlabel('t')
-ylabel('x(t)')
+subplot(4,1,2);
+plot(t,Xt);
+xlabel('t');
+ylabel('x(t)');
 
-subplot(4,1,3)
-stem(n,Yn)
-xlabel('n')
-ylabel('y(n)')
+subplot(4,1,3);
+stem(n,Yn);
+xlabel('n');
+ylabel('y(n)');
 
-subplot(4,1,4)
-plot(t,Yt)
-xlabel('t')
-ylabel('y(t)')
+subplot(4,1,4);
+plot(t,Yt);
+xlabel('t');
+ylabel('y(t)');
