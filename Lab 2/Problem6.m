2a10f52c4add7ef6f3fcdb774c00ea24e8f83741
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-04-27 21:20:12 +0200

    Update Problem6.m

--- Lab 2/Problem6.m
+++ Lab 2/Problem6.m
@@ -3,7 +3,10 @@ n2 = -5:10;
 n3 = 0:20;
 n4 = 0:20;
 
-
+x1 = zeros(size(n1));
+for m = 0 : 10
+    x1 = x1 + (m + 1) .* (dirac(n1 - 2*m) - dirac(n1 - 2*m - 1));
+end
 
 x4_period = [1, 2, 3, 2];
 x4 = [repmat(x4_period, 1, 5) 1];
